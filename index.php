15a0a22b6a1656aa2baadc16b08a588074e25824
Dasha Fomenko <fomenko.dashka@gmail.com>
2018-07-18 12:43:46 +0300

    add some blocks

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400,600,600i,700,700i" rel="stylesheet">
   <link rel="stylesheet" href="<?= asset('styles/main.css') ?>">
 
   <!--[if lt IE 9]>
@@ -16,8 +18,10 @@
   <![endif]-->
 </head>
 
-<body style="background-image:url(<?= asset('images/unsplash.jpg') ?>)">
-  <h1>Barebones</h1>
+<!-- <body style="background-image:url(<?= asset('images/unsplash.jpg') ?>)"> -->
+<body>
+  <button class="btn main-btn">Our services</button>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <script src="<?= asset('scripts/main.js') ?>"></script>
 </body>
 </html>
