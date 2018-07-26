7ed42d2455e4eeff02453128b66d5c5045aa7f9f
Dasha Fomenko <fomenko.dashka@gmail.com>
2018-07-26 14:24:09 +0300

    senior-training page

--- senior-training.php
+++ senior-training.php
@@ -1,36 +1,87 @@
 <?php require_once('header.php'); ?>
 
-<section class="hero-banner home-hero-banner">
-  <div class="hero-gradient"></div>
-  <div class="banner-content">
-    <div class="title-block">
-      <h1><span>A personal trainer</span> in Montreal <br> who will cater to your lifestyle</h1>
+<div class="main-body senior-training-page">
+  <section class="hero-banner senior-hero-banner">
+    <div class="hero-gradient"></div>
+    <div class="banner-content">
+      <div class="title-block">
+        <h1><span>Discover the benefits of</span> <br> Senior Training</h1>
+        <img src="src/images/arrow-down.svg" alt="" class="down-arrow">
+      </div>
     </div>
-    <div class="btn-wrapper">
-      <a href="#" class="btn txt-btn">Call for a <span>free</span> consultation</a>
-    </div>
-    <div class="menu-block">
-      <p>CHOOSE WHAT’S RIGHT FOR YOU:</p>
-      <ul class="banner-menu">
-        <li><a href="#">
-          <span class="icon"><img src="src/images/tht-icons-home.svg" alt=""></span><br>
-          Home Training
-        </a></li>
-        <li><a href="#">
-          <span class="icon"><img src="src/images/tht-icon-senior.svg" alt=""></span><br>
-          Senior Training
-        </a></li>
-        <li><a href="#">
-          <span class="icon"><img src="src/images/golds-logo.png" alt=""></span><br>
-          THT® Gold’s Gyms
-        </a></li>
-        <li><a href="#">
-          <span class="icon"><img src="src/images/share-icon.png" alt=""></span><br>
-          THT® Saint-Lambert Gym
-        </a></li>
-      </ul>
+  </section>
+  <div class="grey-gradient-bg senior-text-container">
+    <div class="container">
+      <section class="senior-text-section white-bg">
+        <div class="row">
+          <div class="col-12 col-md-10 offset-md-1">
+            <div class="senior-text-block">
+              <div class="white-bg s-icon-block">
+                <img src="src/images/lady-dark.svg" alt="">
+              </div>
+              <h3 class="dark">Private home fitness with one of our expert personal trainers can help in reaching your goals of a happier and healthier retirement.</h3>
+              <p>We are taught as a society how to age. We are put on a assembly line and we move along till we reach the end. Well I encourage you to jump of the assembly line and take your health and fitness in your hands and do something about it! Whether it’s to increase the quality of your life, continue playing the sports you enjoy, have energy to play with your children or grand children or enjoy your social and sexual life.</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
-</section>
+  <div class="interesting-facts">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2>Interesting Health Fitness Facts:</h2>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-12 col-md-4">
+          <div class="facts-block">
+            <p>After your <span>20’s</span><br> you lose <span>1 POUND</span><br> of <span>MUSCLE</span> per year</p>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="facts-block">
+            <p>1 pound of<br> <span>MUSCLE LOSS</span><br> <span class="large">=</span> <br> as much as<br> <span>5-8 POUNDS OF FAT<br></span> a year</p>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="facts-block">
+            <p>1 pound of <br> <span>MUSCLE ADDED</span><br> can <span>BURN</span> as much<br> as 5-8 pounds of<br> <span>FAT</span> a year</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-100"></div>
+      <div class="col-12 col-md-10 offset-md-1">
+        <div class="facts-text">
+          <p class="title">Slowing of metabolic rate is associate closer to muscle loss than age.</p>
+          <p>Many medical conditions also known as metabolic syndrome which include obesity, high blood pressure, diabetes,<br> and cardiovascular disease are very closely linked to body fat or loss of lean muscle depends how you want to look at it.</p>
+          <p>If there is <span>1 single thing that you can do</span> that is in your control to prevent sickness and increase quality of life,<br> it’s a <span>customized fitness program</span>.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="grey-gradient-bg what-it-take-container">
+    <div class="container">
+      <section class="what-it-take-section white-bg">
+        <div class="row">
+          <div class="col-12">
+            <div class="what-it-take-block">
+              <img src="src/images/men-st.png" alt="">
+              <div class="text-part">
+                <p class="title">How long does it take to see the <br> results of private training?</p>
+                <p>The results of having a private coach to help you exercise are instant! You will immediately begin to move better, have more energy, think more clearly, and begin to notice less muscle stiffness. Ability to have more range of motion bend and stand with less effort and after a couple of months of training you can expect to gain a precious 1 to 2 pounds of muscle, at the end of 1 year as much as 5 pounds of lean muscle and a loss of 2% body fat.</p>
+                <p class="title">So what does it take to start <br> turning back the clock?</p>
+                <p>Muscle and strength training are your keys to achieving your health goals. You can begin with a training program at any age and start to set back the clock!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+  <div class="btn-wrapper full-width-btn-wrap">
+    <a href="#" class="btn main-btn">Start <i>turning back the clock</i> today <span><img src="src/images/arrow.svg" alt=""></span></a>
+  </div>
+</div>
 
 <?php require_once('footer.php'); ?>
