ba080b9e5c2e78a6dfcec3d507e6c28211a59913
Dasha Fomenko <fomenko.dashka@gmail.com>
2018-07-27 12:42:25 +0300

    locations landing page

---
+++ locations-landing.php
@@ -0,0 +1,98 @@
+<?php require_once('header.php'); ?>
+
+<div class="main-body locations-landing-page">
+  <section class="hero-banner locations-landing-hero-banner">
+    <div class="hero-gradient"></div>
+    <div class="banner-content">
+      <div class="title-block">
+        <h1 class="font-italic font-weight-bold">THT® Locations</h1>
+      </div>
+    </div>
+  </section>
+  <div class="grey-gradient-bg">
+    <section class="locations-landing-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-4">
+            <div class="location-block with-img">
+              <img src="src/images/outside+small1.png" alt="">
+              <div class="location-content">
+                <h3>Gold’s Gym <br> Laval</h3>
+                <p>1164 Rue Desserte Ouest <br> Laval, QC</p>
+              </div>
+              <button class=" btn main-btn full-width">Learn more <span><img src="src/images/arrow.svg" alt=""></span></button>
+            </div>
+          </div>
+          <div class="col-12 col-md-4">
+            <div class="location-block with-img">
+              <img src="src/images/outside+small2.png" alt="">
+              <div class="location-content">
+                <h3>Gold’s Gym <br> Ville Saint Laurent</h3>
+                <p>1164 Rue Desserte Ouest <br> Laval, QC</p>
+              </div>
+              <button class=" btn main-btn full-width">Learn more <span><img src="src/images/arrow.svg" alt=""></span></button>
+            </div>
+          </div>
+          <div class="col-12 col-md-4">
+            <div class="location-block with-img">
+              <img src="src/images/outside+small3.png" alt="">
+              <div class="location-content">
+                <h3>Saint Lambert Gym</h3>
+                <p>1164 Rue Desserte Ouest <br> Laval, QC</p>
+              </div>
+              <button class=" btn main-btn full-width">Learn more <span><img src="src/images/arrow.svg" alt=""></span></button>
+            </div>
+          </div>
+          <div class="w-100"></div>
+          <div class="col-12 col-md-4">
+            <div class="location-block location-block-with-text">
+              <div class="location-content ">
+                <h3>Montreal (Downtown)</h3>
+                <p>1005 Rue Guy <br> Montréal, QC</p>
+              </div>
+              <button class=" btn main-btn full-width">Learn more <span><img src="src/images/arrow.svg" alt=""></span></button>
+            </div>
+          </div>
+          <div class="col-12 col-md-4">
+            <div class="location-block location-block-with-text">
+              <div class="location-content">
+                <h3>South Shore</h3>
+                <p>8525 Avenue San Francisco <br> Brossard, QC</p>
+              </div>
+              <button class=" btn main-btn full-width">Learn more <span><img src="src/images/arrow.svg" alt=""></span></button>
+            </div>
+          </div>
+          <div class="col-12 col-md-4">
+            <div class="location-block location-block-with-text">
+              <div class="location-content">
+                <h3>West Island</h3>
+                <p>3228 Chevremont <br> L'ile bizard, QC</p>
+              </div>
+              <button class=" btn main-btn full-width">Learn more <span><img src="src/images/arrow.svg" alt=""></span></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="landing-home-training">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="landing-home-training-container">
+              <img src="src/images/home-trainer.png" alt="">
+              <div class="content-block">
+                <h3>Enjoy THT® Private Training at Home!</h3>
+                <p>Start achieving your goals by starting with your Private Initial Fitness Assessment with Sam Maniatis today!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div class="btn-wrapper full-width-btn-wrap">
+    <a href="#" class="btn main-btn">Give us a <i>call</i> for more information <span><img src="src/images/arrow.svg" alt=""></span></a>
+  </div>
+</div>
+
+<?php require_once('footer.php'); ?>
