1cda17ea42905ab3cec369456db2753a48d968e4
Dasha Fomenko <fomenko.dashka@gmail.com>
2018-07-27 11:03:47 +0300

    gg landing page

---
+++ gg-landing.php
@@ -0,0 +1,52 @@
+<?php require_once('header.php'); ?>
+
+<div class="main-body gg-landing-page">
+  <section class="hero-banner gg-landing-hero-banner">
+    <div class="hero-gradient"></div>
+    <div class="banner-content">
+      <div class="title-block">
+        <img src="src/images/golds-logo-big.png" alt="" class="hero-logo">
+        <h1 class="font-italic font-weight-bold">THT® Gold’s Gym</h1>
+      </div>
+    </div>
+  </section>
+  <div class="grey-gradient-bg">
+    <section class="gg-landing-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <div class="location-block">
+              <div class="gg-block-landing">
+                <h3>Laval</h3>
+                <a href="gg-laval.php">
+                  <img src="src/images/outside+small1.png" alt="">
+                  <div class="btn gradient-link">Learn more <span><img src="src/images/arrow.svg" alt=""></span></div>
+                </a>
+                <h3>Location</h3>
+                <p>1164 Rue Desserte Ouest<br> Laval, QC<br> H7X 4C9</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6">
+            <div class="location-block">
+              <div class="gg-block-landing">
+                <h3>Ville Saint Laurent</h3>
+                <a href="gg-vsl.php">
+                  <img src="src/images/outside+small2.png" alt="">
+                  <div class="btn gradient-link">Learn more <span><img src="src/images/arrow.svg" alt=""></span></div>
+                </a>
+                <h3>Location</h3>
+                <p>3131 Boulevard Cote-Vertu Ouest<br> Suite C-35, Saint-Laurent, QC<br> H4R 1Y8</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div class="btn-wrapper full-width-btn-wrap">
+    <a href="#" class="btn main-btn">Give us a <i>call</i> for more information <span><img src="src/images/arrow.svg" alt=""></span></a>
+  </div>
+</div>
+
+<?php require_once('footer.php'); ?>
